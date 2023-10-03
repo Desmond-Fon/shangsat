151b901349ac891782542c40152d345ceff4ba4f
Desmond-Fon <desmondfon@yahoo.com>
2023-10-03 13:40:01 +0100

    added hamburger animation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,35 @@
 import logo from '../assets/shangsatLogo.png'
-import hamburger from '../assets/hamburger.svg'
+// import hamburger from '../assets/hamburger.svg'
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 const Nav = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+        // Additional logic for menu toggle goes here
+    };
+
     const navLinkStyle = ({ isActive }) => {
         return {
             color: isActive ? '#F60' : '',
             borderBottom: isActive ? '1px solid #F60' : '',
         };
     };
+
+
     return (<div className="px-[16px] lg:px-[60px] py-[16px] lg:py-[24px] flex justify-between items-center font-roboto sticky top-0 z-40 bg-white">
         <div>
             <img src={logo} alt="" className='w-[70%] lg:w-[100%]' />
         </div>
 
         <div className='lg:hidden'>
-            <img src={hamburger} alt="" />
+            <div className={`hamburger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <div className="line bg-primary"></div>
+                <div className="line bg-secondary"></div>
+                <div className={`line ${!isOpen ? 'bg-primary' : 'bg-secondary'}`}></div>
+            </div>
         </div>
         <div className='lg:flex justify-center items-center gap-7 hidden'>
             <NavLink style={navLinkStyle} to={'/'}>
